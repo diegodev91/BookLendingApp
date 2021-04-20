9a5167a2d737b0cb703be6686e1cb08713783e5f
Diego Castillo <diego.castillo@tektonlabs.com>
2021-04-19 23:14:55 -0500

    Adding useEffect for external request.

--- my-reads/src/App.js
+++ my-reads/src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+Adding import * as api from "./services/BooksAPI";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">Hi Diego</div>;
+  const [books, setBooks] = useState([]);
+
+  const shelfs = [
+    { Id: "currentlyReading", Title: "Currently Reading" },
+    { Id: "wantToRead", Title: "Want to Read" },
+    { Id: "read", Title: "Read" },
+  ];
+
+  useEffect(() => {
+    api.getAll().then((data) => setBooks(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>My Reads</h1>
+      {shelfs.map((shelf) => (
+        <div key={shelf.Title}>
+          <strong>{shelf.Title}</strong>
+          {books
+            .filter((book) => book.shelf === shelf.Id)
+            .map((book) => (
+              <span key={book.title}>{book.title}</span>
+            ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
