de92518c10433dffb6542045ddc3782df69f4818
Diego Castillo <diego.castillo@tektonlabs.com>
2021-04-30 23:58:32 -0500

    Fixing some other stuff

--- my-reads/src/components/shelf/book/book.js
+++ my-reads/src/components/shelf/book/book.js
@@ -4,6 +4,7 @@ import SelectButton from "./select-button/select-button";
 import * as api from "../../../services/BooksAPI";
 
 export default function Book({ book, onBookStatusChanged }) {
+  console.log(book);
   const [bookDetail, setBookDetail] = useState({});
 
   const handleBookStatusChanged = (shelf) => {
@@ -12,22 +13,24 @@ export default function Book({ book, onBookStatusChanged }) {
 
   useEffect((book) => {
     api.get(book).then((data) => {
+      console.log(data);
       setBookDetail(data);
     });
   }, []);
 
   return (
-    <div className={"book"}>
-      {bookDetail.imageLinks && (
-        <img src={bookDetail.imageLinks.thumbnail} alt=""></img>
-      )}
-      <p>{bookDetail.title}</p>
-      {bookDetail.authors &&
-        bookDetail.authors.map((author) => <h6 key={author}>{author}</h6>)}
-      <SelectButton
-        currentShelf={bookDetail.shelf ? bookDetail.shelf : "none"}
-        onBookStatusChanged={handleBookStatusChanged}
-      ></SelectButton>
-    </div>
+    <div>hi</div>
+    // <div className={"book"}>
+    //   {bookDetail.imageLinks && (
+    //     <img src={bookDetail.imageLinks.thumbnail} alt=""></img>
+    //   )}
+    //   <p>{bookDetail.title}</p>
+    //   {bookDetail.authors &&
+    //     bookDetail.authors.map((author) => <h6 key={author}>{author}</h6>)}
+    //   <SelectButton
+    //     currentShelf={bookDetail.shelf ? bookDetail.shelf : "none"}
+    //     onBookStatusChanged={handleBookStatusChanged}
+    //   ></SelectButton>
+    // </div>
   );
 }
--- my-reads/src/components/shelf/shelf.js
+++ my-reads/src/components/shelf/shelf.js
@@ -3,7 +3,6 @@ import React from "react";
 import Book from "./book/book";
 
 export default function Shelf({ books, onBookStatusChanged }) {
-  console.log(books);
   return (
     <div className={"shelf"}>
       {books &&
